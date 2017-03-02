d0bb4b0d612ee6333e218f1be9d54d9e92f159ff
Dlee99 <Dereklee9ab@aol.com>
2017-03-02 17:56:03 -0500

    Corrected SOS stuff

--- src/ZTable.java
+++ src/ZTable.java
@@ -3,17 +3,17 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class ZTable {
-    public static double p(double t1, double t2){
+    public static double p(double t1, double t2) throws FileNotFoundException {
         double std1 = Math.sqrt(t1*(1-t1)), std2 = Math.sqrt(t2*1-t2);
         double z = (t1-t2) / (Math.sqrt(std1) + Math.sqrt(Math.exp(std2)));
-        return CDF_Normal.normp(z);
+        return ztable(z);
     }
     public static double ztable(double z) throws FileNotFoundException {
         double p = .5;
         Scanner txt = new Scanner(new File("Z-Table.txt"));
         int format = (int) (z * 100);
         z = (double) format / 100;
-
+        for(int i = 0; )
         return p;
     }
 }
