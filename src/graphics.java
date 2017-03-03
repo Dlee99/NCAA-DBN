594b3b45214b75d6f4c0d65849a25217c08a8905
Bubb, Matt <mdbubb@users.noreply.github.com>
2017-03-03 18:43:13 -0500

    Update graphics.java

--- src/graphics.java
+++ src/graphics.java
@@ -154,13 +154,3 @@ public class graphics extends JPanel {
 
     }
 }
-/*               y1 = 25; y2 = 25;
-        for (int i = 0, x1 = screen.width - width - 10, x2 = x1 - width; i < 8; i++) {
-            g.drawLine(x1, y1, x2, y2);
-            y1 += 30 + space;
-            g.drawLine(x2, y1, x2, y2);
-            y2 += 30 + space;
-            g.drawLine(x1, y1, x2, y2);
-            y1 += 30 + space;
-            y2 += 30 + space;
-        }*/
