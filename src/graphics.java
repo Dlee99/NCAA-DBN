56d8fb5555157777083a0e811111e990a300debd
dl447 <dereklee9ab@aol.com>
2017-03-06 10:08:18 -0500

    Easier color scheme edits

--- src/graphics.java
+++ src/graphics.java
@@ -3,14 +3,16 @@ import java.awt.*;
 
 public class graphics extends JPanel {
     public void paintComponent(Graphics g) {
+        Color background = Color.DARK_GRAY;
+        Color text = Color.WHITE;
         super.paintComponent(g);
-        this.setBackground(Color.WHITE);
+        this.setBackground(background);
         g.setColor(Color.white);
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
         int space = (screen.height - (30 * 16)) / 20;
         int width = (screen.width - 20) / 12;
         //Left side Round 64 Bracket Lines
-        g.setColor(Color.blue);
+        g.setColor(text);
         int y1 = 10, y2 = 10;
         for (int i = 0, x1 = 10, x2 = x1 + width; i < 16; i++) {
             g.drawLine(x1, y1, x2, y2);
@@ -249,27 +251,27 @@ public class graphics extends JPanel {
 
         int mid = ((screen.width - (width * 7) - 120) + (screen.width - (width * 4) - 10 - width - 100)) / 2;
         JTextField FinalLeft = new JTextField();
-        FinalLeft.setBackground(Color.white);
+        FinalLeft.setBackground(background);
         FinalLeft.setSize(200, 50);
         FinalLeft.setLocation(screen.width - (width * 7) - 120, 240 + 8 * space / 2);
         FinalLeft.setText(Main.FinalTM.get(0));
         FinalLeft.setHorizontalAlignment(JTextField.CENTER);
         Main.frame.add(FinalLeft);
-        FinalLeft.setForeground(Color.blue);
+        FinalLeft.setForeground(text);
 
 
         JTextField FinalRight = new JTextField();
-        FinalRight.setBackground(Color.white);
+        FinalRight.setBackground(background);
         FinalRight.setSize(200, 50);
         FinalRight.setLocation(screen.width - (width * 4) - 10 - width - 100, 240 + 8 * space / 2);
         FinalRight.setText(Main.FinalTM.get(1));
         FinalRight.setHorizontalAlignment(JTextField.CENTER);
         Main.frame.add(FinalRight);
-        FinalRight.setForeground(Color.blue);
+        FinalRight.setForeground(text);
 
 
         JTextField winner = new JTextField();
-        winner.setBackground(Color.white);
+        winner.setBackground(background);
 
         winner.setSize(200, 50);
         winner.setLocation(mid, 240 + 8 * space);
@@ -277,7 +279,7 @@ public class graphics extends JPanel {
         winner.setHorizontalAlignment(JTextField.CENTER);
 
         Main.frame.add(winner);
-        winner.setForeground(Color.blue);
+        winner.setForeground(text);
 
 
         g.setFont(new Font("Times New Roman", Font.BOLD, 35));
